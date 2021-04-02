b15278770242e4cd3b733522be6f35b9d39f83a3
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-04-02 20:23:19 -0300

    Modificação no ex086
    Melhoria de código

--- Exercícios/ex086.py
+++ Exercícios/ex086.py
@@ -4,9 +4,9 @@ matriz = [
     [0, 0, 0]
 ]
 
-for linha, linVal in enumerate(matriz):
-    for col, colVal in enumerate(linVal):
-        linVal[col] = int(input(f'Digite um valor para [{linha}, {col}]: '))
+for linha in range(0, 3):
+    for col in range(0, 3):
+        matriz[linha][col] = int(input(f'Digite um valor para [{linha}, {col}]: '))
 
 print('=' * 30)
 
