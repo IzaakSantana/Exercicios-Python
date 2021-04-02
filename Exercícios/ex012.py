545963abe52a0a44a8ea36f44db1262f11ff5a4a
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-04-02 19:44:55 -0300

    Mudei o nome de uma variável
    Mudança no nome de uma variável para melhor compreensão.

--- Exercícios/ex012.py
+++ Exercícios/ex012.py
@@ -5,5 +5,5 @@
 
 preco = float(input('Qual é o preço do produto? R$'))
 desconto = int(input('Qual é o desconto? %'))
-novop = preco - (preco * desconto / 100)
-print('O produto que custava R${:.2f} agora com desconto de {}% custa R${:.2f}!'.format(preco, desconto, novop))
+novoPreco = preco - (preco * desconto / 100)
+print('O produto que custava R${:.2f} agora com desconto de {}% custa R${:.2f}!'.format(preco, desconto, novoPreco))
