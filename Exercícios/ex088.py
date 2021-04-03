a4007d6d65f2087a925e4790f6d136a0b6769acc
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-04-03 01:27:36 -0300

    Resolução do exercício (incompleto)

--- Exercícios/ex088.py
+++ Exercícios/ex088.py
@@ -1,16 +1,31 @@
-# Desafio 
-# Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
-# O programa vai perguntar quantos jogos serão gerados e vai sortear 6 
-# números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
 
 from random import randint
 from time import sleep
 
-lista = []
+jogos = []
 
 print("=" * 20)
-print("MEGA SENA")
+print(f"{'MEGA SENA':^20}")
 print("=" * 20)
 
-jogos = int(input("Quantos jogos você quer sortear? "))
+xJogos = int(input("Quantos xJogos você quer sortear? "))
+print('\n', '=' * 10, f'SORTEANDO {xJogos} xJogos', '=' * 10)
 
+for j in range(0, xJogos): # cria as temps
+    temp = []
+    for n in range(0, 6): # adiciona valores nelas
+        sleep(0.1)
+        randNum = randint(1, 60)
+
+        if not randNum in temp:
+            temp.append(randNum)
+            temp.sort()
+        else:
+            temp.append(randint(1, 60))
+            temp.sort()
+    lista.append(temp[:])
+    print(f'Jogo {j+1}: {temp}')
+print('=' * 10, f'BOA SORTE!', '=' * 10)
+
+# INCOMPLETO (dps termina de ver o vídeo)
