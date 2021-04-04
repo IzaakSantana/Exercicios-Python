313ad34827e387bd65e96fc04ae287b7247f0297
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-04-04 18:55:16 -0300

    Resolução ex088

--- Exercícios/ex088.py
+++ Exercícios/ex088.py
@@ -1,31 +1,37 @@
-
+# Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+# O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números 
+# entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 
 from random import randint
 from time import sleep
 
 jogos = []
+temp = []
 
 print("=" * 20)
 print(f"{'MEGA SENA':^20}")
 print("=" * 20)
 
-xJogos = int(input("Quantos xJogos você quer sortear? "))
-print('\n', '=' * 10, f'SORTEANDO {xJogos} xJogos', '=' * 10)
+xJogos = int(input("Quantos jogos você quer sortear? "))
+print('\n', '=' * 10, f'SORTEANDO {xJogos} jogos', '=' * 10)
+print()
 
 for j in range(0, xJogos): # cria as temps
-    temp = []
+    temp.clear()
     for n in range(0, 6): # adiciona valores nelas
-        sleep(0.1)
         randNum = randint(1, 60)
 
         if not randNum in temp:
             temp.append(randNum)
-            temp.sort()
         else:
             temp.append(randint(1, 60))
-            temp.sort()
-    lista.append(temp[:])
-    print(f'Jogo {j+1}: {temp}')
-print('=' * 10, f'BOA SORTE!', '=' * 10)
+    
+    temp.sort()
+    jogos.append(temp[:])
 
-# INCOMPLETO (dps termina de ver o vídeo)
+for ind, val in enumerate(jogos):
+    sleep(0.5)
+    print(f'Jogo {ind+1}: {val}')
+
+print()
+print('=' * 10, f'BOA SORTE!', '=' * 10)
