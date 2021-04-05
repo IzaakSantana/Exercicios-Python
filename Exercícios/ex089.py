5a8df832ad38d106bb97e4efe07166c9777611ca
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-04-04 21:40:06 -0300

    Resolução

--- Exercícios/ex089.py
+++ Exercícios/ex089.py
@@ -2,3 +2,44 @@
 # No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as 
 # notas de cada aluno individualmente.
 
+alunos = list()
+aluno = list()
+notas = list()
+
+while True:
+    aluno.append(input('Nome: ').strip().title())
+    notas.append(float(input('Nota 1: ')))
+    notas.append(float(input('Nota 2: ')))
+
+    media = (notas[0] + notas[1]) / 2
+    aluno.append(media)
+    aluno.append(notas[:])
+    alunos.append(aluno[:])
+    aluno.clear()
+    notas.clear()
+
+    resp = input("Quer continuar? [S/N] ")[0].upper()
+    if resp == 'N':
+        break
+
+print('=' * 30)
+
+# 4, 13, 4
+print(f'No. {"NOME":<13} MÉDIA')
+print('-' * 30)
+
+for ind, val in enumerate(alunos):
+    print(f'{ind:<3} {val[0]:<13} {val[1]:>4.1f}')
+
+print('-' * 60)
+
+while True:
+    resp = int(input('Mostrar as notas de qual aluno? (999 interrompe): '))
+
+    if resp == 999:
+        break
+    elif resp <= len(alunos) - 1:
+        print(f'Notas de {alunos[resp][0]}: {alunos[resp][2]}')
+        print('-' * 30)
+
+print('- Finalizado -')
