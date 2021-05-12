f329fc53e5e967200a18cef77bcea505a28a18af
Isaac Santana Souza <isaacsantana.162@gmail.com>
2021-05-12 19:55:48 -0300

    Resolução

--- Exercícios/ex091.py
+++ Exercícios/ex091.py
@@ -2,3 +2,26 @@
 # Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem,
 # sabendo que o vencedor tirou o maior número no dado.
 
+from operator import itemgetter
+from random import randint
+from time import sleep
+
+jogadores = dict()
+ranking = list()
+
+for c in range(1, 5):
+    jogadores[f'jogador{c}'] = randint(1, 6)
+
+print('Valores sorteados:')
+
+for chave, valor in jogadores.items():
+    sleep(0.6)
+    print(f'{chave} tirou {valor} no dado.')
+
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+
+print('\n=== RANKING ===\n')
+
+for chave, valor in enumerate(ranking):
+    sleep(0.6)
+    print(f'{chave + 1}º lugar: {valor[0]} com {valor[1]}')
